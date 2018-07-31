06d3c9e0de6b4e7899cd021309e403c79010a74a
Sean Yu <seansu4you87@gmail.com>
2018-07-30 18:19:49 -0700

    Specs run, sketching out some classes

--- lib/restate.rb
+++ lib/restate.rb
@@ -1,5 +1,25 @@
 require "restate/version"
 
 module Restate
-  # Your code goes here...
+  module DB
+    class Machine; end
+    class State; end
+    class Transition; end
+    class Instance; end
+    class Log; end
+  end
+
+  module DSL
+    class State; end
+    class Transition; end
+    class Graph
+      class Node; end
+      class Edge; end
+    end
+  end
+
+  module Process
+    class PickupWorker; end
+    class LeaseWorker; end
+  end
 end
